784d2304405ad8032268f66a3357b2b6b4bd303a
vlebedenko <slava88.06@mail.ru>
2020-07-26 17:19:40 +0300

    Add tests

--- src/test/java/yandex/blocks/CityRightSideBlock.java
+++ src/test/java/yandex/blocks/CityRightSideBlock.java
@@ -8,8 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class CityRightSideBlock {
     public CityRightSideBlock() {
     }
-    public SelenideElement base = $(".serp-item card__narrow");
-    public SelenideElement city = $(By.xpath("//div[contains(@class, 'serp-title serp-title_type_supertitle')]"));
+    public SelenideElement city = $("[class='entity-search__header'] .serp-title_type_supertitle");
     public SelenideElement weather = $(By.xpath("//ul[contains(@class, 'key-value key-value_layout_default')]" +
             "//b[contains(text(), 'Погода: ')]"));
 }
--- src/test/java/yandex/pages/YandexMain.java
+++ src/test/java/yandex/pages/YandexMain.java
@@ -1,9 +1,11 @@
 package yandex.pages;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import yandex.blocks.CityRightSideBlock;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static org.openqa.selenium.By.xpath;
 
@@ -11,13 +13,21 @@ public class YandexMain {
     public YandexMain() {
     }
     public SelenideElement main = $(xpath("//div[contains(@class, 'col main widgets')]"));
-    public SelenideElement searchField = $("[id='text']");
+    public SelenideElement searchField = $("[name='text']");
     public SelenideElement searchButton = $("[type='submit']");
+    public SelenideElement weatherTitle = $("[id='main_title']");
+    public SelenideElement weatherSubTitle = $("[id='forecast_briefly_title']");
+    public SelenideElement emptyResultMessage = $("[class='misspell__message']");
 
     public CityRightSideBlock cityRightSideBlock = new CityRightSideBlock();
 
     public void waitTillPageLoaded() {
-        main.waitUntil(Condition.visible, 10000);
+        main.waitUntil(visible, 10000);
+    }
+
+    @Step("Проверяем отображаемую URL у {index} элемента в результате поиска")
+    public void checkUrlText(int index, String text) {
+        $(xpath("(//a[contains(@class, 'link_theme_outer')]/b)[" + index + "]")).should(text(text));
     }
 }
 
